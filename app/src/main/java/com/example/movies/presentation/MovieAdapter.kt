892e032042aab90ed5e103338367ccab9f6221df
HansakaHeli <programminghhr66@gmail.com>
2024-01-31 20:48:55 +0530

    Create Movies Adapter

---
+++ app/src/main/java/com/example/movies/presentation/MovieAdapter.kt
@@ -0,0 +1,53 @@
+package com.example.movies.presentation
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.example.movies.R
+import com.example.movies.data.model.Movie
+import com.example.movies.databinding.ListItemBinding
+
+class MovieAdapter(): RecyclerView.Adapter<MovieAdapter.MyViewHolder>() {
+
+    private val movieList = ArrayList<Movie>()
+
+    fun setList(movies: List<Movie>){
+        movieList.clear()
+        movieList.addAll(movies)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding: ListItemBinding = DataBindingUtil.inflate(
+            layoutInflater,
+            R.layout.list_item,
+            parent,
+            false
+        )
+
+        return MyViewHolder(binding)
+    }
+
+    override fun getItemCount(): Int {
+        return movieList.size
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        holder.bind(movieList[position])
+    }
+
+    class MyViewHolder(val binding: ListItemBinding):RecyclerView.ViewHolder(binding.root){
+
+        fun bind(movie:Movie){
+            binding.titleTextView.text = movie.title
+            binding.descTextView.text = movie.overview
+
+            val posterURL = ""
+
+        }
+
+    }
+
+
+}
