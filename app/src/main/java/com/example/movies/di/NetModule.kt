83bf99017369438962dbe7d2a7d2eca32466fcfe
HansakaHeli <programminghhr66@gmail.com>
2024-02-07 23:22:20 +0530

    Create retrofit instance

---
+++ app/src/main/java/com/example/movies/di/NetModule.kt
@@ -0,0 +1,27 @@
+package com.example.movies.di
+
+import com.example.movies.data.api.TMDBService
+import dagger.Module
+import dagger.Provides
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+class NetModule(private val baseUrl: String) {
+
+    @Singleton
+    @Provides
+    fun providesRetrofit(): Retrofit{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(baseUrl)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun providesTMDBService(retrofit: Retrofit):TMDBService{
+        return retrofit.create(TMDBService::class.java)
+    }
+}
